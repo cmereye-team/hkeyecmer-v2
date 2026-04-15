e7b7ac9ab50f6f057da56821ab1a0f9a5a348dc6
谭洁莹 <blacktree@qq.com>
2026-04-15 16:35:50 +0800

    feat: extended/types
    新建页面

---
+++ pages/cataract/intraocular-lens-types.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Author: 谭洁莹
+ * @Date: 2026-04-15 15:30:43
+ * @LastEditTime: 2026-04-15 15:33:04
+ * @FilePath: /pages/cataract/intraocular-lens-types.vue
+ * @Description: 人工晶体比较表
+-->
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'page',
+})
+</script>
+<template>
+  <div>人工晶体比较表</div>
+</template>
+<style lang="scss" scoped></style>
